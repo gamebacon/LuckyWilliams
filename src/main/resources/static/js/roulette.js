d32ca7b5371ea47a0f944bbb562dfb389601a6c0
william with <william.with@hotmail.se>
2022-05-11 09:34:19 +0200

    wok

--- src/main/resources/static/js/roulette.js
+++ src/main/resources/static/js/roulette.js
@@ -1,11 +1,12 @@
 
 const board = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
 const rouletteWheel = document.getElementById('roulette-wheel')
+const rouletteArrow = document.getElementById('roulette-arrow')
 const oneDeg = 9.73
 
 
 function resetRoulette() {
-    rouletteWheel.style.transform = `rotate(0deg)`
+    rouletteArrow.style.transform = `rotate(0deg)`
 }
 
 function spinRoulette(deg = Math.random() * 360) {
@@ -24,6 +25,6 @@ function spinRoulette(deg = Math.random() * 360) {
 
     console.log(`deg: ${deg}, Num: ${num}`)
 
-    rouletteWheel.style.transform = `rotate(${deg}deg)`
+    rouletteArrow.style.transform = `rotate(${deg}deg)`
 
 }
